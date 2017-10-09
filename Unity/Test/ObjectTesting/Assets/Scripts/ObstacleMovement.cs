e10a2cd1825f016b6ea597dfff1707b12d0aacea
JockieN <joakim.nygard@hotmail.com>
2017-10-09 13:20:57 +0200

    Addition: Moving Obstacles
    Moving obstacles, collision

---
+++ Unity/Test/ObjectTesting/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PingPong(Time.time * speed, -5, 5), transform.position.y, transform.position.z);
+    }
+
+    float PingPong(float t, float minLength, float maxLength)
+    {
+        return Mathf.PingPong(t, maxLength - minLength) + minLength;
+    }
+}
